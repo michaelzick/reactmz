50c49a093996f300bf5a790f857cb1fd778ba182
Michael Zick <michaelzick@gmail.com>
2017-05-07 23:56:01 +0000

    ABOUT: converted about to class extends

--- public/js/components/about.jsx
+++ public/js/components/about.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var About = React.createClass ({
-    render: function () {
+class About extends React.Component {
+    render () {
         return (
             <p id="about">
                 {this.props.dataFeed[0].about}
             </p>
         );
     }
-});
+}
 
 ReactDOM.render (
     <About dataFeed={aboutData}/>, document.getElementById('bio-box')
